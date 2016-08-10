e58d371eb38b54300546ffaa093ba31f3ff36327
Elliot Stirling <elliot.stirling@gmail.com>
2016-08-10 23:14:56 +0100

    Generate strings

--- src/test/scala/evolve/DotGraphSpec.scala
+++ src/test/scala/evolve/DotGraphSpec.scala
@@ -1,12 +1,14 @@
 package evolve
 
-import evolve.core.{DotGraph, Instruction, Program}
+import evolve.core.{DotGraph, Generator, Instruction, Program}
+import org.scalacheck.Gen
 import org.scalatest.FlatSpec
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 
 /**
   * Created by ellio on 09/08/2016.
   */
-class DotGraphSpec extends FlatSpec {
+class DotGraphSpec extends FlatSpec with PropertyChecks with GeneratorDrivenPropertyChecks {
   "A known program" should "convert into a DOT representation" in {
     import evolve.functions.BooleanFunctions._
     val p1 = Program( Nop.instructionSize, Seq( Instruction(0) ), 1, 1 )
@@ -18,5 +20,29 @@ class DotGraphSpec extends FlatSpec {
     assert( graph.contains( "Nop" ))
     assert( graph.contains( "\"Input 0\"") )
     assert( graph.contains( "\"Output 0\"") )
+  }
+
+  "Any program" should "roduce a DOT string" in {
+    forAll(Gen.choose[Int](1, 64), Gen.choose[Int](0, 16), Gen.choose[Int](1, 16), Gen.choose[Int](Int.MinValue, Int.MaxValue)) {
+      (size: Int, inputCount: Int, outputCount: Int, seed: Int) => whenever(size >= outputCount && seed != 0) {
+        {
+          import functions.BooleanFunctions._
+          val program = Generator(Nop.instructionSize, size, inputCount, outputCount, seed).shrink
+          assert( DotGraph(program).length > 0 )
+        }
+        {
+          import functions.DoubleFunctions._
+          val program = Generator(Nop.instructionSize, size, inputCount, outputCount, seed).shrink
+          assert( program.used.drop(inputCount).forall( _ == true ))
+          assert( DotGraph(program).length > 0 )
+        }
+        {
+          import functions.IntegerFunctions._
+          val program = Generator(Nop.instructionSize, size, inputCount, outputCount, seed).shrink
+          assert( program.used.drop(inputCount).forall( _ == true ))
+          assert( DotGraph(program).length > 0 )
+        }
+      }
+    }
   }
 }
